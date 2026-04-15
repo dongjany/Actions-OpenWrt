319f5d1eb7e979a82d6b3a253800a9438c99cb00
dongjany <3145255636@qq.com>
2026-04-15 09:12:46 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -11,10 +11,13 @@
 #
 
 # Add a feed source
-echo 'src-git istore https://github.com/linkease/istore;main' >> feeds.conf.default
-echo 'src-git nas https://github.com/linkease/nas-packages.git;master' >> feeds.conf.default
-echo 'src-git nas_luci https://github.com/linkease/nas-packages-luci.git;main' >> feeds.conf.default
-curl -sSL https://raw.githubusercontent.com/chenmozhijin/turboacc/luci/add_turboacc.sh -o add_turboacc.sh && bash add_turboacc.sh
-git clone https://github.com/sbwml/luci-app-ramfree.git package/luci-app-ramfree
-git clone https://github.com/jerrykuku/luci-theme-argon.git package/luci-theme-argon
-git clone https://github.com/ophub/luci-app-amlogic.git package/luci-app-amlogic
+# echo 'src-git istore https://github.com/linkease/istore;main' >> feeds.conf.default
+# echo 'src-git nas https://github.com/linkease/nas-packages.git;master' >> feeds.conf.default
+# echo 'src-git nas_luci https://github.com/linkease/nas-packages-luci.git;main' >> feeds.conf.default
+# curl -sSL https://raw.githubusercontent.com/chenmozhijin/turboacc/luci/add_turboacc.sh -o add_turboacc.sh && bash add_turboacc.sh
+# git clone https://github.com/sbwml/luci-app-ramfree.git package/luci-app-ramfree
+# git clone https://github.com/jerrykuku/luci-theme-argon.git package/luci-theme-argon
+# git clone https://github.com/ophub/luci-app-amlogic.git package/luci-app-amlogic
+
+# Remove unnecessary packages
+# rm -rf package/utils/{ucode,fbtest}
