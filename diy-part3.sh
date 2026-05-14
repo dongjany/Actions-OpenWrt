3e47323196215adedc39c51ec060349b7b1e0380
dongjany <3145255636@qq.com>
2026-05-14 22:41:31 +0800

    Update diy-part3.sh

--- diy-part3.sh
+++ diy-part3.sh
@@ -1,1 +1,8 @@
+# Modify default theme
+sed -i 's/luci-theme-bootstrap/luci-theme-argon/g' feeds/luci/collections/luci-light/Makefile
 
+# Modify hostname
+sed -i 's/OpenWrt/iStoreOS/g' package/base-files/files/bin/config_generate
+
+# 替换终端为bash
+sed -i 's/\/bin\/ash/\/bin\/bash/' package/base-files/files/etc/passwd
