04701f1408e23cf97712ad31d6ee47c591bb167e
dongjany <3145255636@qq.com>
2026-05-15 00:06:38 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -4,13 +4,13 @@ mkdir -p package/ddns-scripts_aliyun
 mkdir -p package/ddns-scripts_dnspod
 
 # 下载 aliyun
-wget https://github.com/Lienol/openwrt-package/main/other/lean/ddns-scripts_aliyun/Makefile -P package/ddns-scripts_aliyun/
-wget https://github.com/Lienol/openwrt-package/main/other/lean/ddns-scripts_aliyun/aliyun.com.json -P package/ddns-scripts_aliyun/
-wget https://github.com/Lienol/openwrt-package/main/other/lean/ddns-scripts_aliyun/update_aliyun_com.sh -P package/ddns-scripts_aliyun/
+wget https://raw.githubusercontent.com/Lienol/openwrt-package/main/other/lean/ddns-scripts_aliyun/Makefile -P package/ddns-scripts_aliyun/
+wget https://raw.githubusercontent.com/Lienol/openwrt-package/main/other/lean/ddns-scripts_aliyun/aliyun.com.json -P package/ddns-scripts_aliyun/
+wget https://raw.githubusercontent.com/Lienol/openwrt-package/main/other/lean/ddns-scripts_aliyun/update_aliyun_com.sh -P package/ddns-scripts_aliyun/
 
 # 下载 dnspod
-wget https://github.com/Lienol/openwrt-package/main/other/lean/ddns-scripts_dnspod/Makefile -P package/ddns-scripts_dnspod/
-wget https://github.com/Lienol/openwrt-package/main/other/lean/ddns-scripts_dnspod/dnspod.cn.json -P package/ddns-scripts_dnspod/
-wget https://github.com/Lienol/openwrt-package/main/other/lean/ddns-scripts_dnspod/dnspod.com.json -P package/ddns-scripts_dnspod/
-wget https://github.com/Lienol/openwrt-package/main/other/lean/ddns-scripts_dnspod/update_dnspod_cn.sh -P package/ddns-scripts_dnspod/
-wget https://github.com/Lienol/openwrt-package/main/other/lean/ddns-scripts_dnspod/update_dnspod_com.sh -P package/ddns-scripts_dnspod/
+wget https://raw.githubusercontent.com/Lienol/openwrt-package/main/other/lean/ddns-scripts_dnspod/Makefile -P package/ddns-scripts_dnspod/
+wget https://raw.githubusercontent.com/Lienol/openwrt-package/main/other/lean/ddns-scripts_dnspod/dnspod.cn.json -P package/ddns-scripts_dnspod/
+wget https://raw.githubusercontent.com/Lienol/openwrt-package/main/other/lean/ddns-scripts_dnspod/dnspod.com.json -P package/ddns-scripts_dnspod/
+wget https://raw.githubusercontent.com/Lienol/openwrt-package/main/other/lean/ddns-scripts_dnspod/update_dnspod_cn.sh -P package/ddns-scripts_dnspod/
+wget https://raw.githubusercontent.com/Lienol/openwrt-package/main/other/lean/ddns-scripts_dnspod/update_dnspod_com.sh -P package/ddns-scripts_dnspod/
