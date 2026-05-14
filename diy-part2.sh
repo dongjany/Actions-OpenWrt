fe8e5031589c595adf747e10ec39a373d00efbf4
dongjany <3145255636@qq.com>
2026-05-15 01:42:48 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -1,1 +1,12 @@
-echo 'src-git kiddin9 https://github.com/kiddin9/op-packages.git;main' >> feeds.conf.default
+#!/bin/bash
+
+# 备份当前配置
+make defconfig
+
+# 自动检测并删除所有 defconfig 报错的软件包
+make defconfig 2>&1 | grep -oP "Package \K[^ ]+" | sort -u | xargs -I {} sed -i '/CONFIG_PACKAGE_{}=/d' .config
+
+# 重新生成正确配置
+make defconfig
+
+echo "✅ 所有有问题的包已删除，配置已修复完成！"
