9e7c15afb971c4c80cf49fddb875dbd19b7b43d2
dongjany <3145255636@qq.com>
2026-05-15 18:12:50 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -10,7 +10,4 @@ echo -e "\n正在修改 kernel Makefile..."
 sed -i 's/^  STAMP_BUILT:=/# &/' package/kernel/linux/Makefile
 sed -i '/^#  STAMP_BUILT/a\  STAMP_BUILT:=$(STAMP_BUILT)_$(shell cat $(LINUX_DIR)/.vermagic)' package/kernel/linux/Makefile
 
-echo -e "\n正在添加 kiddin9 软件源到 feeds.conf.default..."
-echo 'src-git kiddin9 https://github.com/kiddin9/op-packages.git;main' >> feeds.conf.default
-
 echo -e "\n✅ 所有修改完成！"
