8d79b490da5c395bdfb98949f01c50dc3d6130a2
dongjany <3145255636@qq.com>
2026-05-15 18:42:03 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -2,12 +2,8 @@
 
 echo -n "2a67cc4ae7b7c1f0c3b665bec0c6f387" > vermagic
 
-echo -e "\n正在修改 kernel-defaults.mk..."
 sed -i 's/^grep .\+vermagic$/# &/' include/kernel-defaults.mk
 sed -i '/^# grep/a\	cp $(TOPDIR)/vermagic $(LINUX_DIR)/.vermagic' include/kernel-defaults.mk
 
-echo -e "\n正在修改 kernel Makefile..."
 sed -i 's/^  STAMP_BUILT:=/# &/' package/kernel/linux/Makefile
 sed -i '/^#  STAMP_BUILT/a\  STAMP_BUILT:=$(STAMP_BUILT)_$(shell cat $(LINUX_DIR)/.vermagic)' package/kernel/linux/Makefile
-
-echo -e "\n✅ 所有修改完成！"
