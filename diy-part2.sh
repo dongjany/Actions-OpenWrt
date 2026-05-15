b6dd3c755d85c0fbe39002b406146cf0a9b759c6
dongjany <3145255636@qq.com>
2026-05-15 19:19:00 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -5,3 +5,18 @@ git clone https://github.com/ophub/luci-app-amlogic.git package/luci-app-amlogic
 
 rm -rf package/feeds/lienol/other/lean/luci-app-turboacc
 curl -sSL https://raw.githubusercontent.com/chenmozhijin/turboacc/luci/add_turboacc.sh -o add_turboacc.sh && bash add_turboacc.sh
+
+mkdir -p package/ddns-scripts_aliyun
+mkdir -p package/ddns-scripts_dnspod
+
+# 下载 aliyun
+wget https://raw.githubusercontent.com/Lienol/openwrt-package/main/other/lean/ddns-scripts_aliyun/Makefile -P package/ddns-scripts_aliyun/
+wget https://raw.githubusercontent.com/Lienol/openwrt-package/main/other/lean/ddns-scripts_aliyun/aliyun.com.json -P package/ddns-scripts_aliyun/
+wget https://raw.githubusercontent.com/Lienol/openwrt-package/main/other/lean/ddns-scripts_aliyun/update_aliyun_com.sh -P package/ddns-scripts_aliyun/
+
+# 下载 dnspod
+wget https://raw.githubusercontent.com/Lienol/openwrt-package/main/other/lean/ddns-scripts_dnspod/Makefile -P package/ddns-scripts_dnspod/
+wget https://raw.githubusercontent.com/Lienol/openwrt-package/main/other/lean/ddns-scripts_dnspod/dnspod.cn.json -P package/ddns-scripts_dnspod/
+wget https://raw.githubusercontent.com/Lienol/openwrt-package/main/other/lean/ddns-scripts_dnspod/dnspod.com.json -P package/ddns-scripts_dnspod/
+wget https://raw.githubusercontent.com/Lienol/openwrt-package/main/other/lean/ddns-scripts_dnspod/update_dnspod_cn.sh -P package/ddns-scripts_dnspod/
+wget https://raw.githubusercontent.com/Lienol/openwrt-package/main/other/lean/ddns-scripts_dnspod/update_dnspod_com.sh -P package/ddns-scripts_dnspod/
