ec4d21a87f32dc262401f4abe2c65edb0d76429d
dongjany <3145255636@qq.com>
2026-05-15 14:39:57 +0800

    Update diy-part3.sh

--- diy-part3.sh
+++ diy-part3.sh
@@ -1,3 +1,14 @@
+#!/bin/bash
+
+# Set the default LAN IP address
+default_ip="192.168.1.1"
+ip_regex="^((25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.){3}(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)$"
+# Override default IP if a valid custom IP is provided as the first argument
+[[ -n "${1}" && "${1}" != "${default_ip}" && "${1}" =~ ${ip_regex} ]] && {
+    echo "Modify default IP address to: ${1}"
+    sed -i "/lan) ipad=\${ipaddr:-/s/\${ipaddr:-\"[^\"]*\"}/\${ipaddr:-\"${1}\"}/" package/base-files/*/bin/config_generate
+}
+
 # Modify default theme
 sed -i 's/luci-theme-bootstrap/luci-theme-argon/g' feeds/luci/collections/luci-light/Makefile
 
@@ -6,3 +17,27 @@ sed -i 's/OpenWrt/iStoreOS/g' package/base-files/files/bin/config_generate
 
 # 替换终端为bash
 sed -i 's/\/bin\/ash/\/bin\/bash/' package/base-files/files/etc/passwd
+
+# Set the default password for the 'root' user (change empty password to 'password')
+sed -i 's/root:::0:99999:7:::/root:$1$V4UetPzk$CYXluq4wUazHjmCDBCqXF.::0:99999:7:::/g' package/base-files/files/etc/shadow
+
+# Append source repository information to etc/openwrt_release
+sed -i "s|DISTRIB_REVISION='.*'|DISTRIB_REVISION='R$(date +%Y.%m.%d)'|g" package/base-files/files/etc/openwrt_release
+echo "DISTRIB_SOURCEREPO='github.com/Lienol/openwrt'" >> package/base-files/files/etc/openwrt_release
+echo "DISTRIB_SOURCECODE='Lienol'" >> package/base-files/files/etc/openwrt_release
+echo "DISTRIB_SOURCEBRANCH='24.10'" >> package/base-files/files/etc/openwrt_release
+
+# Configure ccache for build acceleration
+# Remove existing ccache settings
+sed -i '/CONFIG_DEVEL/d' .config
+sed -i '/CONFIG_CCACHE/d' .config
+# Apply new ccache configuration
+if [[ "${2}" == "true" ]]; then
+    echo "CONFIG_DEVEL=y" >>.config
+    echo "CONFIG_CCACHE=y" >>.config
+    echo 'CONFIG_CCACHE_DIR="$(TOPDIR)/.ccache"' >>.config
+else
+    echo '# CONFIG_DEVEL is not set' >>.config
+    echo "# CONFIG_CCACHE is not set" >>.config
+    echo 'CONFIG_CCACHE_DIR=""' >>.config
+fi
