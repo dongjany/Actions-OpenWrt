d85a25f541ce0df193fd5ef6f51af42917923b07
dongjany <3145255636@qq.com>
2026-05-15 19:22:04 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -9,6 +9,7 @@ sed -i 's/^  STAMP_BUILT:=/# &/' package/kernel/linux/Makefile
 sed -i '/^#  STAMP_BUILT/a\  STAMP_BUILT:=$(STAMP_BUILT)_$(shell cat $(LINUX_DIR)/.vermagic)' package/kernel/linux/Makefile
 
 # Add a feed source
+sed -i '/src-git lienol/d' feeds.conf.default
 echo 'src-git istore https://github.com/linkease/istore;main' >> feeds.conf.default
 echo 'src-git nas https://github.com/linkease/nas-packages.git;master' >> feeds.conf.default
 echo 'src-git nas_luci https://github.com/linkease/nas-packages-luci.git;main' >> feeds.conf.default
