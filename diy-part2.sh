92d7f44d892162feeebe23a1cc0f3da4556aeac7
dongjany <3145255636@qq.com>
2026-05-15 19:15:26 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -3,4 +3,5 @@
 git clone https://github.com/jerrykuku/luci-theme-argon.git package/luci-theme-argon
 git clone https://github.com/ophub/luci-app-amlogic.git package/luci-app-amlogic
 
+rm -rf package/feeds/lienol/other/lean/luci-app-turboacc
 curl -sSL https://raw.githubusercontent.com/chenmozhijin/turboacc/luci/add_turboacc.sh -o add_turboacc.sh && bash add_turboacc.sh
